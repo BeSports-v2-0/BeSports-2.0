e9a33be810b566afdb85367f64f1034ffcd605c0
Rafael Bollit <rafa.bollit@gmail.com>
2020-02-17 17:38:04 +0100

    api called and filter

--- starter-code/routes/profile.routes.js
+++ starter-code/routes/profile.routes.js
@@ -2,11 +2,22 @@ const express = require('express')
 const router = express.Router()
 const User = require('../models/User.model')
 const uploadCloud = require('../configs/cloudinary.config')
+const apiHandle = require("../services/APIHandler")
+
+const api = new apiHandle()
 
 router.get("/", (req, res, ) => {
-  res.render("profile/profile", {
-    user: req.user
-  })
+  api.getAllInfo()
+    .then(res => {
+      console.log(res.data)
+      res.render("profile/profile", {
+        user: req.user, apidata: res.data.title
+      })
+
+
+    })
+    .catch(err => console.log(err))
+
 })
 
 router.post('/', uploadCloud.single('phototoupload'), (req, res, next) => {
@@ -15,10 +26,10 @@ router.post('/', uploadCloud.single('phototoupload'), (req, res, next) => {
     path: req.file.secure_url
   }
   User.findByIdAndUpdate(req.user._id, {
-      profilePicture
-    }, {
-      new: true
-    })
+    profilePicture
+  }, {
+    new: true
+  })
     .then(x => res.redirect('/profile'))
     .catch(err => console.log('error con la foto', err))
 })
